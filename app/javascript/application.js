8e1beb2af1c493a92d852c6640e46164ce61bd8e
brunoocarvalhoo <bruno.carvalho310303@gmail.com>
2023-11-05 00:51:18 -0300

    Cria tela root para usuários que não estão logados

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,28 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+
+document.addEventListener("DOMContentLoaded", function () {
+    const themeToggle = document.getElementById("bd-theme");
+    const themeDropdown = document.querySelector(".dropdown-menu");
+
+    // Função para alternar o tema
+    function toggleTheme(theme) {
+        document.body.dataset.bsTheme = theme;
+        // Você pode adicionar outras ações relacionadas ao tema aqui, se necessário.
+    }
+
+    // Manipula os cliques nos itens do dropdown
+    themeDropdown.addEventListener("click", (event) => {
+        if (event.target.dataset.bsThemeValue) {
+            const selectedTheme = event.target.dataset.bsThemeValue;
+            toggleTheme(selectedTheme);
+
+            // Atualiza o ícone do botão do dropdown
+            const themeIconActive = document.querySelector(".theme-icon-active");
+            themeIconActive.innerHTML = event.target.querySelector("svg").outerHTML;
+
+            // Fecha o dropdown
+            themeToggle.setAttribute("aria-expanded", "false");
+            themeToggle.blur();
+        }
+    });
+});
