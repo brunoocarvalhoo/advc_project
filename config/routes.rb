a6df8f81cec34ba3b393fbebc2bfaa0026819931
brunoocarvalhoo <bruno.carvalho310303@gmail.com>
2023-11-05 00:54:56 -0300

    Cria rotas lógica de rota padrão e novas rotas do sistema

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,16 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "home#index"
+  authenticated :user do
+    root to: 'home#athletes', as: :authenticated_root
+  end
+
+  unauthenticated do
+    root to: 'home#index', as: :unauthenticated_root
+  end
+
+  get 'home/events', to: 'home#events', as: 'events'
+  get 'users/users_manager', to: 'users#users_manager', as: 'users_manager'
+  get 'users/delete_user', to: 'users#delete_user', as: 'delete_user'
+  patch 'users/update_user', to: 'users#update_user', as: 'update_user'
 end
